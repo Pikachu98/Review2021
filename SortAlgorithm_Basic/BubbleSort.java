aecd688563a60e4b1bbbcb1d3428256d1cea0739
Qianwen Zhang <326200153@qq.com>
2021-10-06 11:24:12 +0800

    Bubble Sort

--- SortAlgorithm_Basic/BubbleSort.java
+++ SortAlgorithm_Basic/BubbleSort.java
@@ -1,5 +1,21 @@
 package Model.SortAlgorithm_Basic;
 
-// 每次查看
+// 每次都查看两个相邻元素是否逆序，逆序则交换两者的顺序，如果是从大到小的话，最大的就会被冒泡冒到最后面去
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        for (int a: arr) {
+            System.out.println(a);
+        }
+    }
 }
